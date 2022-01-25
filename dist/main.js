0ea53e2bc01bc370cd7d7109646867375e716d5e
Beki <bereketadego@gmail.com>
2022-01-25 15:18:05 +0300

    added modules

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("// eslint-disable-next-line consistent-return\nexports.stringLength = string => {\n  if (string.length > 0 && string.length < 10) {\n    return string.length;\n  }\n};\n\nexports.reverseString = string => string.split('').reverse().join('');\n\nexports.capitalizeString = string => string.charAt(0).toUpperCase() + string.slice(1);\n\n//# sourceURL=webpack://microverse-template/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"](0, __webpack_exports__);
+/******/ 	
+/******/ })()
+;
